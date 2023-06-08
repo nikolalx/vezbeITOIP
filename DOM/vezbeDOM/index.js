14e0927a2130cefb89bb6ba2c44765ad788fc289
nikolalx <nikilx123@gmail.com>
2023-06-08 22:28:43 +0200

    vezbe za DOM

---
+++ DOM/vezbeDOM/index.js
@@ -0,0 +1,7 @@
+// Napisati f-ju koja za uneti broj okrece cifre u broju. Rezultat ispisuje u novom inputu. (4532 -> 2354)
+// Napisati f-ju koja iz stringa uklanja sve sto se nalazi u zagradama. Rezultat ispisuje u paragrafu.
+
+// Napisati f-ju koja iz forme kupi broj i proverava da li je broj prost. Rezultat funkcije se ispisuje u paragrafu.
+
+// Napisati f-ju koja iz textarea uzima tekst i posle svake tacke ubacuje space ako on nedostaje.
+
