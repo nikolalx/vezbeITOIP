cbe6d247f99ec913176dbde97e24cb2b8cd1aa0c
nikolalx <nikilx123@gmail.com>
2023-06-14 18:14:25 +0200

    .

---
+++ DOM/vezbeDOM/domaci/zadatak4.js
@@ -0,0 +1,5 @@
+// Napisati kod koji refaktorise tekst tako da posle svake tacke na kraju 
+// recenice unese razmak za novu recenicu koja sledi.
+// (opciono 1. Pokusajte da resite zadatak za sve interpunkcijske znake kojima terminisemo recenicu)
+// (opciono 2. Razmislite kako biste napisali funkciju koja ce regulisati problem ukoliko se recenica zavrsava sa ...)
+
